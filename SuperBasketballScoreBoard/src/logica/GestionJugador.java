007958c82d855ffba03de56162a9fa919e583c1a
krizzm94 <kmilo_stuar@hotmail.com>
2013-10-31 12:03:15 -0500

    creacion de la clase "GestionJugador"

---
+++ SuperBasketballScoreBoard/src/logica/GestionJugador.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package logica;
+
+/**
+ * @author Cristhian Camilo Medina
+ *
+ */
+public class GestionJugador {
+
+
+private Torneo torneo;
+private Equipo equipo;
+private Jugador jugador;
+
+
+
+
+
+}
