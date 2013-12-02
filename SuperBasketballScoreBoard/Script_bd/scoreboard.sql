f724bcf02f76aca1e495ed6adf90b025054c84ef
wolfran1212 <wolfran2008@gmail.com>
2013-12-02 12:04:29 -0500

    Modificacion Script base de datos

--- SuperBasketballScoreBoard/Script_bd/scoreboard.sql
+++ SuperBasketballScoreBoard/Script_bd/scoreboard.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generaciÃ³n: 19-11-2013 a las 00:13:08
--- VersiÃ³n del servidor: 5.5.25a
--- VersiÃ³n de PHP: 5.4.4
+-- Tiempo de generación: 02-12-2013 a las 18:00:51
+-- Versión del servidor: 5.5.25a
+-- Versión de PHP: 5.4.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `scoreboard`
 --
+
+CREATE DATABASE scoreboard;
 
 -- --------------------------------------------------------
 
@@ -40,19 +42,6 @@ CREATE TABLE IF NOT EXISTS `equipo` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `falta`
---
-
-CREATE TABLE IF NOT EXISTS `falta` (
-  `id` int(1) NOT NULL,
-  `id_jugador` int(2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_jugador` (`id_jugador`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `jugador`
 --
 
@@ -66,6 +55,7 @@ CREATE TABLE IF NOT EXISTS `jugador` (
   KEY `nombre_torneo` (`nombre_torneo`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=7 ;
 
+
 -- --------------------------------------------------------
 
 --
@@ -78,9 +68,25 @@ CREATE TABLE IF NOT EXISTS `partido` (
   `fecha` varchar(12) COLLATE latin1_spanish_ci DEFAULT NULL,
   `hora` varchar(5) COLLATE latin1_spanish_ci DEFAULT NULL,
   `nombre_torneo` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
   KEY `nombre_torneo` (`nombre_torneo`),
   KEY `visitante` (`visitante`),
   KEY `local` (`local`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resultado`
+--
+
+CREATE TABLE IF NOT EXISTS `resultado` (
+  `ganador` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `pun_local` int(3) NOT NULL,
+  `pun_vist` int(3) NOT NULL,
+  `id_partido` int(10) NOT NULL,
+  UNIQUE KEY `id_partido` (`id_partido`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
 -- --------------------------------------------------------
@@ -95,6 +101,7 @@ CREATE TABLE IF NOT EXISTS `torneo` (
   PRIMARY KEY (`nombre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -106,17 +113,11 @@ ALTER TABLE `equipo`
   ADD CONSTRAINT `equipo_ibfk_1` FOREIGN KEY (`nombre_torneo`) REFERENCES `torneo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `falta`
---
-ALTER TABLE `falta`
-  ADD CONSTRAINT `falta_ibfk_1` FOREIGN KEY (`id_jugador`) REFERENCES `jugador` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Filtros para la tabla `jugador`
 --
 ALTER TABLE `jugador`
-  ADD CONSTRAINT `jugador_ibfk_2` FOREIGN KEY (`nombre_torneo`) REFERENCES `torneo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `jugador_ibfk_1` FOREIGN KEY (`nombre_equipo`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `jugador_ibfk_1` FOREIGN KEY (`nombre_equipo`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `jugador_ibfk_2` FOREIGN KEY (`nombre_torneo`) REFERENCES `torneo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `partido`
@@ -125,6 +126,12 @@ ALTER TABLE `partido`
   ADD CONSTRAINT `partido_ibfk_1` FOREIGN KEY (`local`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `partido_ibfk_2` FOREIGN KEY (`visitante`) REFERENCES `equipo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `partido_ibfk_3` FOREIGN KEY (`nombre_torneo`) REFERENCES `torneo` (`nombre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `resultado`
+--
+ALTER TABLE `resultado`
+  ADD CONSTRAINT `resultado_ibfk_1` FOREIGN KEY (`id_partido`) REFERENCES `partido` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
