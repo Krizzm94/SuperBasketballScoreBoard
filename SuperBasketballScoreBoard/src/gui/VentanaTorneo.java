bff6d616f2a54c6cfa6b7247b1f5ee47446817c3
krizzm94 <kmilo_stuar@hotmail.com>
2013-12-03 17:26:03 -0500

    cambio en los mensajes emergentes.

--- SuperBasketballScoreBoard/src/gui/VentanaTorneo.java
+++ SuperBasketballScoreBoard/src/gui/VentanaTorneo.java
@@ -228,18 +228,5 @@ public class VentanaTorneo {
 	/**
 	 * Launch the application.
 	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					VentanaTorneo window = new VentanaTorneo(null);
-//					window.frame.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		});
-//	}
-
 
 }
